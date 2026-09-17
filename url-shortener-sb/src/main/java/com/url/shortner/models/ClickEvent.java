54d58a904ca2f8d177f70c78916a8fa7018aa5dd
Harmeet Singh <hs260457@gmail.com>
2026-09-17 19:16:04 +0530

    fix: switch GenerationType.AUTO to IDENTITY for PostgreSQL compatibility

--- url-shortener-sb/src/main/java/com/url/shortner/models/ClickEvent.java
+++ url-shortener-sb/src/main/java/com/url/shortner/models/ClickEvent.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Data
 public class ClickEvent {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDateTime clickDate;
