b27e8a4ef69cd5d6ffcec7bebcfd99bc05c35618
Harmeet Singh <hs260457@gmail.com>
2026-09-17 19:32:36 +0530

    fix: revert GenerationType to AUTO to fix database constraint issues on Aiven PostgreSQL

--- url-shortener-sb/src/main/java/com/url/shortner/models/User.java
+++ url-shortener-sb/src/main/java/com/url/shortner/models/User.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String email;
     private String username;
